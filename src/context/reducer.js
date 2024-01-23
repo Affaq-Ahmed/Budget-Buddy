9437408de2400b29d7fe1fb6bbf203ddbe0493e8
Affaq-Ahmed <aaawan165@gmail.com>
2024-01-23 22:57:25 +0500

    Add transaction and delete transaction functionality to context reducer

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,5 +1,12 @@
 const contextReducer = (state, action) => {
-  
+	switch (action.type) {
+		case "DELETE_TRANSACTION":
+			return state.filter((item) => item.id !== action.payload);
+		case "ADD_TRANSACTION":
+			return [action.payload, ...state];
+		default:
+			return state;
+	}
 };
 
 export default contextReducer;
