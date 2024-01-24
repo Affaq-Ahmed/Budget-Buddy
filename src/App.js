e91ab96886edad3c43e8f152c0f72cabbb052119
Affaq-Ahmed <aaawan165@gmail.com>
2024-01-24 20:45:21 +0500

    Refactor grid layout and add responsive behavior

--- src/App.js
+++ src/App.js
@@ -1,31 +1,45 @@
 import React from "react";
 import Details from "./components/Details/Details";
-import { Grid } from "@mui/material";
+import { Grid, useMediaQuery } from "@mui/material";
 import Main from "./components/Main/Main";
 
 const App = () => {
+	const isSmallScreen = useMediaQuery("(max-width:600px)");
+
 	return (
 		<div>
 			<Grid
 				container
-				spacing={1}
+				spacing={isSmallScreen ? 6 : 2}
 				alignItems="center"
 				justifyContent="center"
-				style={{ height: "100vh" }}
-				sx={{
-					"& > *": {
-						margin: (theme) => theme.spacing(1),
-					},
-					height: "100vh",
-				}}
+				style={{ height: "100vh", marginTop: isSmallScreen ? "20px" : "0px" }}
 			>
-				<Grid item xs={12} sm={4}>
+				<Grid
+					item
+					xs={11}
+					sm={4}
+					style={{ display: isSmallScreen ? "none" : "block" }}
+				>
 					<Details title="Income" />
 				</Grid>
-				<Grid item xs={12} sm={3} sx={{ paddingBottom: { sm: "5%" } }}>
+				<Grid item xs={11} sm={3} sx={{ paddingBottom: { sm: "5%" } }}>
 					<Main />
 				</Grid>
-				<Grid item xs={12} sm={4}>
+				<Grid
+					item
+					xs={11}
+					sm={4}
+					style={{ display: isSmallScreen ? "block" : "none" }}
+				>
+					<Details title="Income" />
+				</Grid>
+				<Grid
+					item
+					xs={11}
+					sm={4}
+					style={{ paddingBlockEnd: isSmallScreen ? "100px" : "0px" }}
+				>
 					<Details title="Expense" />
 				</Grid>
 			</Grid>
