ab0ba920bb5f1ec39640dffc6b14f1c32982bd20
Affaq-Ahmed <aaawan165@gmail.com>
2024-01-24 15:41:34 +0500

    Refactor context.js and reducer.js to persist transactions in localStorage

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,7 @@
 import { createContext, useReducer } from "react";
 import contextReducer from "./reducer";
 
-const initialState = [];
+const initialState = JSON.parse(localStorage.getItem("transactions")) || [];
 
 export const ExpenseTrackerContext = createContext(initialState);
 
@@ -13,9 +13,15 @@ export const Provider = ({ children }) => {
 	const addTransaction = (transaction) =>
 		dispatch({ type: "ADD_TRANSACTION", payload: transaction });
 
+	const balance = transactions.reduce(
+		(acc, currVal) =>
+			currVal.type === "Expense" ? acc - currVal.amount : acc + currVal.amount,
+		0
+	);
+
 	return (
 		<ExpenseTrackerContext.Provider
-			value={{ deleteTransaction, addTransaction, transactions }}
+			value={{ deleteTransaction, addTransaction, transactions, balance }}
 		>
 			{children}
 		</ExpenseTrackerContext.Provider>
